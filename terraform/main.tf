2907c38fc88325e8daa47526084dac38faaa81ea
Sadiq <acube201@gmail.com>
2025-11-22 21:29:03 -0500

    Initial commit: Project 2 CI/CD + Terraform + Azure

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
